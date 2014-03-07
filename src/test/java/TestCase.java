114404846fa6ef2b6e3e08170dbff2c4ca9e2096
Clement Pang <me@clementpang.com>
2014-03-06 23:39:10 -0800

    more test cases

--- src/test/java/TestCase.java
+++ src/test/java/TestCase.java
@@ -11,8 +11,21 @@ import static org.junit.Assert.assertEquals;
 public class TestCase {
 
   @Test
-  public void test() {
+  public void test1() {
     // test should return 0, now it stalls forever
     assertEquals(0, ByteArrayUtil.compareUnsigned(new byte[]{-1, 0, 2, 4, 5}, new byte[]{-1, 0, 2, 4, 5}));
+  }
+
+  @Test
+  public void test2() {
+    // test should return 0, now it stalls forever
+    assertEquals(-1, ByteArrayUtil.compareUnsigned(new byte[]{-1, 0, 2, 4, 5}, new byte[]{-1, 0, 2, 4, 6}));
+  }
+
+
+  @Test
+  public void test3() {
+    // test should return 0, now it stalls forever
+    assertEquals(1, ByteArrayUtil.compareUnsigned(new byte[]{-1, 0, 2, 4, 6}, new byte[]{-1, 0, 2, 4, 5}));
   }
 }
